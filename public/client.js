8801991448ad738a2f9ad6bdd1380f71ceb23468
KieuLong <kieulong1401@gmail.com>
2024-07-04 20:51:34 +0900

    movement

--- public/client.js
+++ public/client.js
@@ -5,63 +5,22 @@ const socket = io()
 socket.on('connect', () => {
     var id = socket.id
     console.log(id)
-    
-    
+
     const menu = document.querySelector('#menu')
     const joinBtn = document.querySelector('#joinBtn')
     const canvas = document.querySelector('#canvas')
-    
+
     const myGame = new Game(canvas)
-    
+
     var frontendPlayers = {}
-    var directions = new Set()
     var animationId
-    
-    
-    if(Object.keys(frontendPlayers).includes(id)) {
-        menu.classList.add('hide')
-    }
-    
-    
+
     function addPlayer() {
         if (!Object.keys(frontendPlayers).includes(socket.id)) {
             socket.emit('addPlayer', 'long')
             menu.classList.add('hide')
         }
     }
-    function updatePlayerPosition() {
-        const myCharacter = frontendPlayers[id]
-
-        const isDiagonalDirection = directions.size == 2
-        const speed = isDiagonalDirection ? Math.sqrt(Math.max(5, 10 - Math.floor(this.point / 500)))/2 : Math.max(5, 10 - Math.floor(this.point / 500))
-        const nextPosition = {
-            x: this.position.x,
-            y: this.position.y
-        }
-
-        directions.forEach(direction => {
-            switch(direction) {
-                case 'up':
-                    nextPosition.y -= speed
-                    break
-                case 'left':
-                    nextPosition.x -= speed
-                    break
-                case 'right':
-                    nextPosition.x += speed
-                    break
-                case 'down':
-                    nextPosition.y += speed
-                    break
-            } 
-        })
-
-        socket.emit('updatePlayer', {
-            ...myCharacter,
-            position: nextPosition
-        })
-    }
-    
     function renderPlayers() {
         Object.keys(frontendPlayers).forEach((e) => {
             const player = frontendPlayers[e]
@@ -73,62 +32,32 @@ socket.on('connect', () => {
 
         myGame.render()
         renderPlayers()
-        updatePlayerPosition()
     }
-    
-    
+
     joinBtn.addEventListener('click', addPlayer)
-    
+
     window.addEventListener('keydown', (event) => {
-        switch(event.code) {
-            case 'KeyW':
-                if(!directions.has('down')) directions.add('up')
-                break
-            case 'KeyA':
-                if(!directions.has('right')) directions.add('left')
-                break
-            case 'KeyD':
-                if(!directions.has('left')) directions.add('right')
-                break
-            case 'KeyS':
-                if(!directions.has('up')) directions.add('down')
-                break
-        }
+        socket.emit('keydown', event.code)
     })
     window.addEventListener('keyup', (event) => {
-        switch(event.code) {
-            case 'KeyW':
-                directions.delete('up')
-                break
-            case 'KeyA':
-                directions.delete('left')
-                break
-            case 'KeyD':
-                directions.delete('right')
-                break
-            case 'KeyS':
-                directions.delete('down')
-                break
-        }
+        socket.emit('keyup', event.code)
     })
-    
-    
-    
+
     socket.on('updatePlayers', (backendPlayers) => {
         const frontendPlayerIds = Object.keys(frontendPlayers)
         const backendPlayerIds = Object.keys(backendPlayers)
-    
-        frontendPlayerIds.forEach(e => {
-            if(!backendPlayerIds.includes(e)) {
+
+        frontendPlayerIds.forEach((e) => {
+            if (!backendPlayerIds.includes(e)) {
                 delete frontendPlayers[e]
             }
         })
-        backendPlayerIds.forEach(e => {
+        backendPlayerIds.forEach((e) => {
             const player = new Player(backendPlayers[e])
             frontendPlayers[e] = player
+            console.log(frontendPlayers[e].position)
         })
     })
-    
+
     animate()
 })
-
--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const httpServer = createServer(app)
 const io = new Server(httpServer, { pingInterval: 2000, pingTimeout: 4000 })
 
 const colors = ['#39FF14', '#FF6EC7', '#00FFFF', '#FFFF00', '#FFA500']
+const gameMaxSize = 500
 var players = {}
-
+var directions = {}
+var intervalId
 
 function getRandomColor() {
     const randomIndex = Math.floor(Math.random() * colors.length)
@@ -26,7 +28,7 @@ function sendPlayers() {
     io.emit('updatePlayers', players)
 }
 function updatePlayer(id, value) {
-    if(value == null) {
+    if (value == null) {
         delete players[id]
     } else {
         players[id] = value
@@ -34,7 +36,55 @@ function updatePlayer(id, value) {
 
     sendPlayers()
 }
+function updatePlayerPosition() {
+    Object.keys(directions).forEach((id) => {
+        if (!players[id]) return
 
+        var speed = Math.max(5, 10 - Math.floor(players[id].point / 500))
+
+        const isDiagonalDirection = directions[id].size == 2
+        if (isDiagonalDirection) speed = Math.sqrt(speed ** 2 / 2)
+
+        const updatedPosition = players[id].position
+
+        Array.from(directions[id].entries()).forEach((direction) => {
+            switch (direction[0]) {
+                case 'up':
+                    if (updatedPosition.y - speed < 0) {
+                        updatedPosition.y = 0
+                    } else {
+                        updatedPosition.y -= speed
+                    }
+                    break
+                case 'left':
+                    if (updatedPosition.x - speed < 0) {
+                        updatedPosition.x = 0
+                    } else {
+                        updatedPosition.x -= speed
+                    }
+                    break
+                case 'right':
+                    if (updatedPosition.x + speed > gameMaxSize) {
+                        updatedPosition.x = gameMaxSize
+                    } else {
+                        updatedPosition.x += speed
+                    }
+                    break
+                case 'down':
+                    if (updatedPosition.y + speed > gameMaxSize) {
+                        updatedPosition.y = gameMaxSize
+                    } else {
+                        updatedPosition.y += speed
+                    }
+                    break
+            }
+        })
+        updatePlayer(id, {
+            ...players[id],
+            position: updatedPosition,
+        })
+    })
+}
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -45,10 +95,12 @@ io.on('connection', (socket) => {
     const id = socket.id
 
     sendPlayers()
-    socket.on('disconnect', (reason) => {
+
+    socket.on('disconnect', () => {
         if (players[id]) {
             colors.push(players[id].color)
             updatePlayer(id, null)
+            delete directions[id]
         }
     })
     socket.on('addPlayer', (name) => {
@@ -58,13 +110,52 @@ io.on('connection', (socket) => {
             point: 0,
             name,
         })
+        directions[id] = new Set()
     })
-    socket.on('updatePlayer', (player) => {
-        updatePlayer(id, player)
+    socket.on('keydown', (keycode) => {
+        if (!directions[id]) return
+        switch (keycode) {
+            case 'KeyW':
+                if (directions[id].has('down')) return
+                directions[id].add('up')
+                break
+            case 'KeyA':
+                if (directions[id].has('right')) return
+                directions[id].add('left')
+                break
+            case 'KeyD':
+                if (directions[id].has('left')) return
+                directions[id].add('right')
+                break
+            case 'KeyS':
+                if (directions[id].has('up')) return
+                directions[id].add('down')
+                break
+        }
     })
+    socket.on('keyup', (keycode) => {
+        if (!directions[id]) return
+        switch (keycode) {
+            case 'KeyW':
+                directions[id].delete('up')
+                break
+            case 'KeyA':
+                directions[id].delete('left')
+                break
+            case 'KeyD':
+                directions[id].delete('right')
+                break
+            case 'KeyS':
+                directions[id].delete('down')
+                break
+        }
+    })
+
+    clearInterval(intervalId)
+    intervalId = setInterval(() => {
+        updatePlayerPosition()
+    }, 16)
 })
-
-
 
 httpServer.listen(post, () => {
     console.log(`server running in post ${post}`)
