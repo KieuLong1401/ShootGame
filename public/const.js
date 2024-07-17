11cdd902c9d3ab8295588581bfdd40a0caf10c84
KieuLong <kieulong1401@gmail.com>
2024-07-17 17:20:10 +0900

    change constant

--- public/const.js
+++ public/const.js
@@ -14,19 +14,19 @@ export const PLAYER_SPEED = 15
 export const PLAYER_SIZE = 40
 export const PLAYER_HP = 100
 
-export const SHOOT_DELAY = 400
+export const SHOOT_DELAY = 350
 export const BULLET_DAMAGE = 20
 export const BULLET_SPEED = 20
 export const BULLET_WIDTH = 20
 export const BULLET_HEIGHT = 10
 export const BULLET_RANGE_LIMIT = 1000
 
-export const HEAL_AFTER_GET_HIT = 5000
-export const HEAL_AMOUNT = 0.5
+export const HEAL_AFTER_GET_HIT = 7000
+export const HEAL_AMOUNT = 0.3
 
 export const KILL_ANNOUNCE_TIMEOUT = 3000
 
-export const POLYGON_RADIUS = 100
+export const POLYGON_RADIUS = 350
 export const POLYGONAL_TYPE = 6
 
 export const JOYSTICK_SIZE = 50
